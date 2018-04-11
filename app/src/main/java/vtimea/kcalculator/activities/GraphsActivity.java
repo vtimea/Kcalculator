b3fda95afbc63f03ba53f632cdc062ef9bfc3da3
VAJNATIMI\vajnatimi <vajnatimi@gmail.com>
2018-04-11 23:32:46 +0200

    graphs activity: add nav drawer

--- app/src/main/java/vtimea/kcalculator/activities/GraphsActivity.java
+++ app/src/main/java/vtimea/kcalculator/activities/GraphsActivity.java
@@ -1,15 +1,78 @@
 package vtimea.kcalculator.activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import vtimea.kcalculator.R;
 
 public class GraphsActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphs);
+
+        mDrawerLayout = findViewById(R.id.home_activity_drawer_layout);
+
+//        init navigation drawer
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.getMenu().getItem(1).setChecked(true);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        Intent intent;
+                        switch (item.getOrder()){
+                            //home
+                            case 0:
+                                intent = new Intent(getBaseContext(), HomeActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                                startActivity(intent);
+                                return true;
+                            //graphs
+                            case 1:
+                                //stay on graphs activity
+                                return true;
+                            //settings
+                            case 2:
+                                intent = new Intent(getBaseContext(), SettingsActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                                startActivity(intent);
+                                return true;
+                            default:
+                                //stay on the activity
+                                return true;
+                        }
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if(mDrawerLayout.isDrawerOpen(GravityCompat.START))
+                    mDrawerLayout.closeDrawers();
+                else
+                    mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

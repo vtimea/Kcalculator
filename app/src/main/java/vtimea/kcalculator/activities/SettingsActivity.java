9605687772e795af452db10d7aabd5b9ca1f59e4
VAJNATIMI\vajnatimi <vajnatimi@gmail.com>
2018-05-12 15:48:52 +0200

    settings activity done

--- app/src/main/java/vtimea/kcalculator/activities/SettingsActivity.java
+++ app/src/main/java/vtimea/kcalculator/activities/SettingsActivity.java
@@ -2,6 +2,8 @@ package vtimea.kcalculator.activities;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,33 +12,52 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import java.util.ArrayList;
+import java.util.List;
 
 import vtimea.kcalculator.R;
 
 public class SettingsActivity extends AppCompatActivity {
+    private final String PREF_CALS = "Calories";
 
     private DrawerLayout mDrawerLayout;
-    private Switch aSwitch;
     private TextView etCals;
+    private Button btnCalculate;
+
+    private enum ActivityLevel{
+        SEDENTARY(1.2), MODERATELY_ACTIVE(1.3), ACTIVE(1.4);
+        private final double value;
+
+        private ActivityLevel(double value) {
+            this.value = value;
+        }
+
+        public double getValue() {
+            return value;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         initNavDrawer();
-        initSwitch();
-        etCals = findViewById(R.id.etSettingsCalories);
+        initButton();
+        initCalories();
     }
 
     @Override
@@ -94,51 +115,106 @@ public class SettingsActivity extends AppCompatActivity {
         );
     }
 
-    private void initSwitch(){
-        aSwitch = findViewById(R.id.swCalcAuto);
+    private void initButton(){
+        btnCalculate = findViewById(R.id.btnCalcAuto);
 
-        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                //if it's checked
-                if(b){
-                    LayoutInflater inflater = getLayoutInflater();
-                    View alertLayout = inflater.inflate(R.layout.dialog_calc_calories, null);
-                    final Spinner spGender = alertLayout.findViewById(R.id.spGender);
-                    final EditText etHeight = alertLayout.findViewById(R.id.etHeight);
-                    final EditText etWeight = alertLayout.findViewById(R.id.etWeight);
-                    final EditText etAge = alertLayout.findViewById(R.id.etAge);
-                    final Spinner spActivity = alertLayout.findViewById(R.id.spActivityLevel);
+            public void onClick(View view) {
+                LayoutInflater inflater = getLayoutInflater();
+                View alertLayout = inflater.inflate(R.layout.dialog_calc_calories, null);
 
-                    AlertDialog.Builder alert = new AlertDialog.Builder(SettingsActivity.this);
-                    alert.setTitle("Calculate your calories");
-                    alert.setView(alertLayout);
-                    alert.setCancelable(false);
-                    alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which){
-                            //cancel
-                        }
-                    });
+                //init components on dialog
+                final Spinner spGender = alertLayout.findViewById(R.id.spGender);
+                final EditText etHeight = alertLayout.findViewById(R.id.etHeight);
+                final EditText etWeight = alertLayout.findViewById(R.id.etWeight);
+                final EditText etAge = alertLayout.findViewById(R.id.etAge);
+                final Spinner spActivity = alertLayout.findViewById(R.id.spActivityLevel);
 
-                    alert.setPositiveButton("Done", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            int height = Integer.parseInt(etHeight.getText().toString());
-                            int weight = Integer.parseInt(etWeight.getText().toString());
-                            int age = Integer.parseInt(etAge.getText().toString());
-                            int calories = calculateCalories(false, height, weight, age, ActivityLevel.MODERATELY_ACTIVE);
-                            etCals.setText(Integer.toString(calories));
-                            etCals.setEnabled(false);
-                            Toast.makeText(getBaseContext(), "Done", Toast.LENGTH_SHORT).show();
+
+                //init alert dialog
+                AlertDialog.Builder alert = new AlertDialog.Builder(SettingsActivity.this);
+                alert.setTitle(R.string.alert_title);
+                alert.setView(alertLayout);
+                alert.setCancelable(false);
+
+                //CANCEL
+                alert.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which){
+                        //cancel
+                    }
+                });
+
+                //OK
+                alert.setPositiveButton(R.string.done, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //getting the numbers from the edittexts
+                        int height = Integer.parseInt(etHeight.getText().toString());
+                        int weight = Integer.parseInt(etWeight.getText().toString());
+                        int age = Integer.parseInt(etAge.getText().toString());
+                        boolean isMale = spGender.getSelectedItemPosition() == 0;
+                        ActivityLevel activityLevel;
+                        switch (spActivity.getSelectedItemPosition()){
+                            case 0:
+                                activityLevel = ActivityLevel.SEDENTARY;
+                                break;
+                            case 1:
+                                activityLevel = ActivityLevel.MODERATELY_ACTIVE;
+                                break;
+                            case 2:
+                                activityLevel = ActivityLevel.ACTIVE;
+                                break;
+                            default:
+                                activityLevel = ActivityLevel.SEDENTARY;
+                                break;
                         }
-                    });
-                    AlertDialog dialog = alert.create();
-                    dialog.show();
-                }
-                else{
-                    etCals.setEnabled(true);
-                }
+
+                        //calculate and save calories
+                        int calories = calculateCalories(isMale, height, weight, age, activityLevel);
+                        saveCaloresToPrefs(calories);
+                        etCals.setText(Integer.toString(calories));
+                        etCals.clearFocus();
+                    }
+                });
+
+                //show dialog
+                AlertDialog dialog = alert.create();
+                dialog.show();
+            }
+        });
+    }
+
+    private void initCalories(){
+        etCals = findViewById(R.id.etSettingsCalories);
+
+        //if the user previously saved a calorie intake then show that, else show 2000
+        int calories = getCaloriesFromPrefs();
+        if(calories < 0) {
+            etCals.setText("2000");
+        }
+        else{
+            etCals.setText(Integer.toString(calories));
+        }
+
+        etCals.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //do nothing
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(etCals.getText().toString().equals(""))
+                    return;
+                int calories = Integer.parseInt(etCals.getText().toString());
+                saveCaloresToPrefs(calories);
             }
         });
     }
@@ -159,17 +235,18 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
-    private enum ActivityLevel{
-        SEDENTARY(1.2), MODERATELY_ACTIVE(1.3), ACTIVE(1.4);
-        private final double value;
+    private void saveCaloresToPrefs(int calories){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.remove(PREF_CALS);
+        editor.putInt(PREF_CALS,calories);
+        editor.apply();
+    }
 
-        private ActivityLevel(double value) {
-            this.value = value;
-        }
-
-        public double getValue() {
-            return value;
-        }
+    private int getCaloriesFromPrefs(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        int calories = preferences.getInt(PREF_CALS, -1);
+        return calories;
     }
 
 }
